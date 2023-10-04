0e908137836cc769809ee6c2bf611768749b2694
JacobSeidner <jseidne1@binghamton.edu>
2023-10-04 14:47:14 -0400

    for real thsi time

--- practice-for-ch-rails-goal-app-long-practice-main/goal_app/app/models/user.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal_app/app/models/user.rb
@@ -2,17 +2,6 @@ class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: {minumum: 6, allow_nil: true}
-    # validate :strong_password
-
-    attr_reader :password
-
-    # def strong_password
-    #     if password.length >= 6 || password == allow_nil
-    #         return true
-    #     end
-    #     return false
-    # end
-
     #figvapber
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
