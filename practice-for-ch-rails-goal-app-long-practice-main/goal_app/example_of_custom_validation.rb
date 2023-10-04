34ecea14cbd2606a3184b032ea944d312fbdc7cb
JacobSeidner <jseidne1@binghamton.edu>
2023-10-04 14:46:25 -0400

    added example_of_custom_validation file

---
+++ practice-for-ch-rails-goal-app-long-practice-main/goal_app/example_of_custom_validation.rb
@@ -0,0 +1,10 @@
+    # validate :strong_password
+
+    attr_reader :password
+
+    # def strong_password
+    #     if password.length >= 6 || password == allow_nil
+    #         return true
+    #     end
+    #     return false
+    # end
